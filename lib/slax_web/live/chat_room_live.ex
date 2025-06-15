f4af1b33fef18ec9bb61e92eec2d7e5b5da04fa6
Cesar Jolibois <diazcd85@gmail.com>
2025-06-15 16:04:06 -0400

    list all rooms in sidebar

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -6,13 +6,35 @@ defmodule SlaxWeb.ChatRoomLive do
 
   def render(assigns) do
     ~H"""
+    <div class="flex flex-col shrink-0 w-64 bg-slate-100">
+      <div class="flex justify-between items-center shrink-0 h-16 border-b border-slate-300 px-4">
+        <div class="flex flex-col gap-1.5">
+          <h1 class="text-lg font-bold text-gray-800">
+            Slax
+          </h1>
+        </div>
+      </div>
+
+      <div class="mt-4 overflow-auto">
+        <div class="flex items-center h-8 px-3">
+          <span class="ml-2 leading-none font-medium text-sm">Rooms</span>
+        </div>
+
+        <div id="rooms-list">
+          <.room_link :for={room <- @rooms} room={room} active={room.id == @room.id} />
+        </div>
+      </div>
+    </div>
     <div class="flex flex-col grow shadow-lg">
       <div class="flex justify-between items-center shrink-0 h-16 bg-white border-b border-slate-300 px-4">
         <div class="flex flex-col gap-1.5">
           <h1 class="text-sm font-bold leading-none">
             {@room.name}
           </h1>
-          <div class={["text-xs leading-none h-3.5", @hide_topic? && "text-slate-600"]}phx-click="toggle-topic">
+          <div
+            class={["text-xs leading-none h-3.5", @hide_topic? && "text-slate-600"]}
+            phx-click="toggle-topic"
+          >
             <%= if @hide_topic? do %>
               [Topic hidden]
             <% else %>
@@ -25,13 +47,30 @@ defmodule SlaxWeb.ChatRoomLive do
     """
   end
 
+  attr :active, :boolean, required: true
+  attr :room, Room, required: true
+
+  defp room_link(assigns) do
+    ~H"""
+    <a
+      class={["flex items-center h-8 text-sm pl-8 pr-3", (@active && "bg-slate-300") || "hover:bg-slate-300"]}
+      href="#"
+    >
+      <.icon name="hero-hashtag" class="h-4 w-4"/>
+      <span class={["ml-2 leading-none", @active && "font-bold"]} >
+        {@room.name}
+      </span>
+    </a>
+    """
+  end
+
   def mount(_params, _session, socket) do
-    # This should be move to a context
-    room = Repo.all(Room) |> List.first()
-    {:ok, assign(socket, [hide_topic?: false, room: room])}
+    rooms = Repo.all(Room)
+    room = rooms |> List.first()
+    {:ok, assign(socket, hide_topic?: false, room: room, rooms: rooms)}
   end
 
   def handle_event("toggle-topic", _unsigned_params, socket) do
-    {:noreply, update(socket, :hide_topic?, &!&1)}
+    {:noreply, update(socket, :hide_topic?, &(!&1))}
   end
 end
