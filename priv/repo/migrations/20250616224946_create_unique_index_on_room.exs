8dc8b474e4497ad26201628cce3e8ba9ac06914f
Cesar Jolibois <diazcd85@gmail.com>
2025-06-16 18:56:55 -0400

    added validate room name uniqueness

---
+++ priv/repo/migrations/20250616224946_create_unique_index_on_room.exs
@@ -0,0 +1,11 @@
+defmodule Slax.Repo.Migrations.CreateUniqueIndexOnRoom do
+  use Ecto.Migration
+
+  def up do
+    create unique_index(:rooms, :name)
+  end
+
+  def down do
+    drop unique_index(:rooms , :name)
+  end
+end
