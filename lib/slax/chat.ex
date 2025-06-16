328a8417fb7ca0d80d037bcc0549c07b012dddb6
Cesar Jolibois <diazcd85@gmail.com>
2025-06-16 13:13:54 -0400

    added functions to create and update room to chat context

--- lib/slax/chat.ex
+++ lib/slax/chat.ex
@@ -7,4 +7,16 @@ defmodule Slax.Chat do
   def list_rooms(), do: from(Room, order_by: [asc: :name]) |> Repo.all()
 
   def get_room!(id), do: Repo.get!(Room, id)
+
+  def create_room(attrs) do
+    attrs |> put_change() |> Repo.insert()
+  end
+
+  def update_room(room, attrs) do
+    room |> put_change(attrs) |> Repo.update()
+  end
+
+  defp put_change(room \\ %Room{}, attr) do
+    room |> Room.changeset(attr)
+  end
 end
--- lib/slax/chat/room.ex
+++ lib/slax/chat/room.ex
@@ -10,9 +10,14 @@ defmodule Slax.Chat.Room do
   end
 
   @doc false
-  def changeset(room, attrs) do
+  def changeset(%__MODULE__{} = room, attrs) do
     room
     |> cast(attrs, [:name, :topic])
-    |> validate_required([:name, :topic])
+    |> validate_required(:name)
+    |> validate_length(:name, max: 80)
+    |> validate_format(:name, ~r/\A[a-z0-9]+\z/,
+      message: "can only contain lowercase letter, numbers and dashes"
+    )
+    |> validate_length(:topic, max: 200)
   end
 end
